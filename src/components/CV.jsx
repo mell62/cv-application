7502849350c49a432a35188eb52c0ed0c5b2ed8c
Mridhul <mridhul67@gmail.com>
2025-01-20 00:33:52 +0530

    Refactor: Split CV content into different components

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -4,24 +4,36 @@ import "../styles/modern-normalize.css";
 export default function CV({ data }) {
   return (
     <div className="cv-sheet">
-      <div className="general-info-cv">
-        <h1 className="full-name-cv">{data.fullName}</h1>
-        <div className="general-sub">
-          <div className="general-sub-1">
-            <div className="email-cv">{data.email}</div>
-            <div className="phone-cv">{data.phone}</div>
-          </div>
-          <div className="general-sub-2">
-            <div className="linkedin-cv">{data.linkedin}</div>
-            <div className="github-cv">{data.github}</div>
-          </div>
+      <GeneralInfo data={data} />
+      <About data={data} />
+    </div>
+  );
+}
+
+function GeneralInfo({ data }) {
+  return (
+    <div className="general-info-cv">
+      <h1 className="full-name-cv">{data.fullName}</h1>
+      <div className="general-sub">
+        <div className="general-sub-1">
+          <div className="email-cv">{data.email}</div>
+          <div className="phone-cv">{data.phone}</div>
+        </div>
+        <div className="general-sub-2">
+          <div className="linkedin-cv">{data.linkedin}</div>
+          <div className="github-cv">{data.github}</div>
         </div>
       </div>
-      <div className="main-info-cv">
-        <div className="about-cv">
-          <h1 className="about-header">About</h1>
-          {data.about}
-        </div>
+    </div>
+  );
+}
+
+function About({ data }) {
+  return (
+    <div className="main-info-cv">
+      <div className="about-cv">
+        <h1 className="about-header">About</h1>
+        {data.about}
       </div>
     </div>
   );
